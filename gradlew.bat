e02ecf864c3159b336b8848009695c483aa73a4d
leec <2672724132@qq.com>
2026-02-14 14:41:57 +0800

    修复: gradlew JVM参数引号问题

--- gradlew.bat
+++ gradlew.bat
@@ -7,7 +7,7 @@ set DIRNAME=%~dp0
 set APP_BASE_NAME=%~n0
 set APP_HOME=%DIRNAME%
 
-set DEFAULT_JVM_OPTS="-Xmx64m" "-Xms64m"
+set DEFAULT_JVM_OPTS=-Xmx64m -Xms64m
 
 set CLASSPATH=%APP_HOME%\gradle\wrapper\gradle-wrapper.jar
 
